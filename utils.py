c855b2c7c98d7fdfea9eeb64050ca6e7c1f8da8f
Sai Kiran Patro <saikiranpatro198@gmail.com>
2024-05-16 04:27:08 +0530

    Mobile UI

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta
 def time_ago(date):
-  now = datetime.utcnow()
+  now = datetime.now()
   diff = now - date
-
+  print("diff",diff)
   seconds = diff.seconds
   minutes = diff.seconds / 60
   hours = minutes / 60
   days = hours / 24
   weeks = days / 7
-
+  print("seconds",seconds," ", minutes," ",hours," "," ", days," ",weeks)
   # Check for seconds and minutes first
   if seconds < 60:
     return f"{int(seconds) } seconds ago"
