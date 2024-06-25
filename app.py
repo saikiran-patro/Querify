67d4a9757b6cd15ff05a9316c8a54e93b5d7bf25
Sai Kiran Patro <saikiranpatro198@gmail.com>
2024-06-25 23:58:31 +0530

    updated file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from Website import create_app
 from utils import time_ago
-
+from flask import Flask
 app=create_app()
 
 app.jinja_env.filters['time_ago'] = time_ago
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
     
